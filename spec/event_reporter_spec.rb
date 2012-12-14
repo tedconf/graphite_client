c99d6196ca7ae375c9f37b816895550229541729
Matthew Trost <mrtrost@gmail.com>
2012-12-14 16:01:24 -0500

    Add specs to test HTTPS requests

--- spec/event_reporter_spec.rb
+++ spec/event_reporter_spec.rb
@@ -5,7 +5,9 @@ describe "GraphiteClient::EventReporter" do
   before do
     @url         = 'http://example.com/path'
     @uri         = URI(@url)
-    @event       = {'what' => 'test', 'tags' => 'test', 'data' => 'test'}
+    @https_url   = 'https://example.com/path'
+    @https_uri   = URI(@https_url)
+    @event       = {:what => 'test', :tags => 'test', :data => 'test'}
     @basic_auth  = {:username => 'test', :password => 'test'}
   end
 
@@ -36,6 +38,29 @@ describe "GraphiteClient::EventReporter" do
       req.should_receive(:body=).with(@event.to_json)
       http.should_receive(:request).with(req)
       event_reporter = GraphiteClient::EventReporter.new(@url, basic_auth: @basic_auth)
+      event_reporter.report(@event)
+    end
+  end
+
+  context "over SSL" do
+    it "should create an HTTPS connection and POST to it" do
+      Net::HTTP.should_receive(:new).with(@https_uri.host, @https_uri.port).and_return(http = mock())
+      Net::HTTP::Post.should_receive(:new).with(@https_uri.request_uri).and_return(req = mock())
+      http.should_receive(:use_ssl=).with(true)
+      req.should_receive(:body=).with(@event.to_json)
+      http.should_receive(:request).with(req)
+      event_reporter = GraphiteClient::EventReporter.new(@https_url)
+      event_reporter.report(@event)
+    end
+
+    it "should create an HTTPS connection and POST to it, using basic auth" do
+      Net::HTTP.should_receive(:new).with(@https_uri.host, @https_uri.port).and_return(http = mock())
+      Net::HTTP::Post.should_receive(:new).with(@https_uri.request_uri).and_return(req = mock())
+      http.should_receive(:use_ssl=).with(true)
+      req.should_receive(:basic_auth).with(@basic_auth[:username], @basic_auth[:password])
+      req.should_receive(:body=).with(@event.to_json)
+      http.should_receive(:request).with(req)
+      event_reporter = GraphiteClient::EventReporter.new(@https_url, basic_auth: @basic_auth)
       event_reporter.report(@event)
     end
   end
